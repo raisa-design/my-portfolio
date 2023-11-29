04330ac653a2125b6dc0b84061be85f2f488429d
Raisa_Dev <73198136+raisa-design@users.noreply.github.com>
2023-11-29 19:19:19 -0300

    feat: add initial html structure and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+
+import { Header } from './components/header/header'
+import './components/styles/home.scss'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <h1>Olá Mundo</h1>
+    <main className='container'>
+        <Header/>      
+        <div className='experience'>
+          <h3>Experince</h3>
+          <p>3 years of experience as a fullstack developer</p>
+          <div className='experience-time'>
+
+          </div>
+          <div className='infos'>
+            <h3>Languages</h3>
+            <div className='languages-info'>
+              <span>En - intermediary</span>
+              <span>PT-BR - Native Speaker</span>
+            </div>
+            <h3>Education</h3>
+            <div className='education-info'>
+              <span>emoji</span>
+              <span></span>
+            </div>
+          </div>
+          <div className="butons">
+            <div className='social'>
+
+            </div>
+            <button>contact me</button>
+          </div>
+        </div>
     </main>
   )
 }
